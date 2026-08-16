6a9a2b8924defff340b9d046d497d841eb05e55a
Leiliani <Leilianifreitas302@gmail.com>
2026-08-16 16:06:28 -0300

    feat: implementa contador com useState

--- App.js
+++ App.js
@@ -4,26 +4,17 @@ import { Text, View, Button, StyleSheet } from "react-native";
 export default function App() {
   const [contagem, setContagem] = useState(0);
 
-  const precos = [10, 15, 20, 25, 30, 35];
-
-  let contador = 0;
-
   // contador = contador + 1;
 
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>"Ola, Turma de INF204!"</Text>
-
-      <Text style={styles.titulo}>"Preços:"</Text>
-      {precos.filter((preco) => preco > 20)
-        .map((preco) => <Text style={styles.titulo}>{preco}</Text>)}
-
-      {/* <Text style={styles.titulo}>"CONTADOR:"</Text>
+      <Text style={styles.titulo}>"Contagem atual:"</Text>
       <Text style={styles.numero}>{contagem}</Text>
       <Button
         title="Incrementar +1"
         onPress={() => setContagem(contagem + 1)}
-      /> */}
+      /> 
     </View>
   );
 }
