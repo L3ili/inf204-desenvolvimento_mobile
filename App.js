76a4a760ea203ea4423367f1bc9eba322b7e5e7c
Leiliani <Leilianifreitas302@gmail.com>
2026-08-24 01:08:49 -0300

    feat: adiciona style

--- App.js
+++ App.js
@@ -15,3 +15,38 @@ style={styles.avatar}
 </View>
 );
 }
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: "#F5F5F5",
+justifyContent: "center",
+alignItems: "center",
+},
+cartao: {
+backgroundColor: "#FFFFFF",
+padding: 30,
+borderRadius: 15,
+alignItems: "center",
+shadowColor: "#000",
+shadowOpacity: 0.1,
+shadowRadius: 10,
+elevation: 5,
+width: "80%",
+},
+avatar: {
+width: 120,
+height: 120,
+borderRadius: 60,
+marginBottom: 15,
+},
+nomeUsuario: {
+fontSize: 22,
+fontWeight: "bold",
+color: "#14325A", // ProBlue
+},
+profissao: {
+fontSize: 16,
+color: "#505050", // ProGray
+marginBottom: 20,
+}
+});
