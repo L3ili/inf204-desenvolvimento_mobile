6e8bc27a132f56f7e9da7993f89241258db27c1b
Leiliani <Leilianifreitas302@gmail.com>
2026-08-24 00:53:20 -0300

    feat: cria cartão perfil de usuário

--- App.js
+++ App.js
@@ -1,65 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 export default function App() {
-// Criando um Array de Objetos em JavaScript puro
-const tarefas = [
-{ id: 1, descricao: "Estudar ES6+", concluida: true },
-{ id: 2, descricao: "Configurar ambiente Expo", concluida: true },
-{ id: 3, descricao: "Entender o funcionamento do JSX", concluida: false },
-{ id: 4, descricao: "Finalizar Roteiro de Pratica 02", concluida: false },
-];
-const pendente = tarefas.filter((tarefa) => !tarefa.concluida );
-// Renderizacao Condicional: Se concluida exibe "OK" senao exibe "PENDENTE" 
 return (
 <View style={styles.container}>
-<Text style={styles.titulo}>Lista de Tarefas</Text>
-{tarefas.map((tarefa) => (
-<View key={tarefa.id} style={styles.card}>
-<Text style={styles.textoTarefa}>
-{/* Renderizacao Condicional: Se concluida exibe [OK] */}
-{tarefa.concluida ? "[OK] " : "[PENDENTE] "}
-{tarefa.descricao}
-</Text>
+<View style={styles.cartao}>
+{/* Imagem Remota: Obrigatorio definir width e height no estilo */}
+<Image
+source={{ uri: "..." }}
+style={styles.avatar}
+/>
+<Text style={styles.nomeUsuario}>Joao Vitor</Text>
+<Text style={styles.profissao}>Engenheiro de Software</Text>
 </View>
-))}
-
-<Text style={styles.titulo}>Lista de Tarefas Pendentes</Text>
-{pendente.map((tarefa) => (
-<View key={tarefa.id} style={styles.card}>
-<Text style={styles.textoTarefa}>
-{tarefa.concluida ? "OK " : "PENDENTE"}
-{tarefa.descricao}
-</Text>
-</View>
-))}
 </View>
 );
 }
-const styles = StyleSheet.create({
-container: {
-flex: 1,
-backgroundColor: "#f5f5f5",
-paddingTop: 50,
-paddingHorizontal: 20,
-},
-titulo: {
-fontSize: 24,
-fontWeight: "bold",
-marginBottom: 20,
-color: "#380474",
-},
-card: {
-backgroundColor: "#7a4caf",
-padding: 15,
-borderRadius: 8,
-marginBottom: 10,
-shadowColor: "#000",
-shadowOpacity: 0.1,
-shadowRadius: 4,
-elevation: 2,
-},
-textoTarefa: {
-fontSize: 16,
-color: "#111010",
-},
-});
