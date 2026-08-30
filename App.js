fdc50470cd710904c98945d120c556685ef1e3ee
Leiliani <Leilianifreitas302@gmail.com>
2026-08-30 14:20:36 -0300

    feat: implementa ScrollView

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, ScrollView } from "react-native";
 export default function App() {
+const pessoas = [
+  { id: 1, nome: "João Vitor", profissao: "Engenheiro de Software" },
+  { id: 2, nome: "Leiliani", profissao: "Analista de Sistemas" },
+  { id: 3, nome: "Gabrielle", profissao: "Desenvolvedora" }
+];
 const [nome, setNome] = useState("Joao Vitor");
 const [seguindo, setSeguindo] = useState(true);
 
 return (
-<View style={styles.container}>
-<View style={styles.cartao}>
-<Image source={{ uri: "..." }} style={styles.avatar} />
-<Text style={styles.nomeUsuario}>{nome}</Text>
-<Text style={styles.profissao}>Engenheiro de Software</Text>
+ <ScrollView contentContainerStyle={styles.container}>
+    {pessoas.map((pessoa) => (
+      <View key={pessoa.id} style={styles.cartao}>
+        <Image source={{ uri: "..." }} style={styles.avatar} />
+        <Text style={styles.nomeUsuario}>{pessoa.nome}</Text>
+        <Text style={styles.profissao}>{pessoa.profissao}</Text>
+      </View>
+    ))}
+
 {/* Botao Customizado */}
 <TouchableOpacity style={[styles.botao, seguindo ? styles.botaoDesativado : null]} activeOpacity={0.7} onPress={() => {
 alert("Seguindo " + nome);
@@ -24,9 +33,7 @@ placeholder="Alterar nome..."
 value={nome}
 onChangeText={(texto) => setNome(texto)}
 />
-
-</View>
-</View>
+  </ScrollView>
 );
 }
 const styles = StyleSheet.create({
